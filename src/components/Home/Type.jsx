c978e9316a00b618e3b8b858c27fe15ccf0f2670
ilyaveruk <ilyaveruk@gmail.com>
2023-07-20 20:59:30 +0300

    Changed some looks

--- src/components/Home/Type.jsx
+++ src/components/Home/Type.jsx
@@ -8,8 +8,7 @@ function Type() {
                 strings: [
                     "Software Developer",
                     "Freelancer",
-                    "MERN Stack Developer",
-                    "Open Source Contributor",
+                    "BSc Software Engineering",
                 ],
                 autoStart: true,
                 loop: true,
