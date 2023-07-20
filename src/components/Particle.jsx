8bfa01ab011270a6c91be3679e04783b604f2d51
ilyaveruk <ilyaveruk@gmail.com>
2023-07-20 20:45:55 +0300

    Added interactive particles

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useCallback} from "react";
 import Particles from "react-tsparticles";
+import {loadSlim} from "tsparticles-slim";
+
 
 function Particle() {
+
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     return (
         <Particles
-            id="tsparticles"
+            id="particles"
+            init={particlesInit}
+            loaded={particlesLoaded}
             params={{
                 particles: {
                     number: {
